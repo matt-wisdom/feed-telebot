fc94cb980576829ad70c44813d99ce52c56a4479
cRyp70s <matthewwisdom11@gmail.com>
2022-07-20 01:11:20 +0100

    Messages for bot

---
+++ feedbot/messages.py
@@ -0,0 +1,33 @@
+# WELCOME MESSAGES
+welcome_old = "Hello"
+welcome_new = "Hello"
+registered = "What do you want to do"
+
+# ACTIONS BUTTONS
+subscribe = "Subscribe to daily updates"
+unsubscribe = "Unsubscribe from daily updates"
+sub_feed = "Subscribe/Unsubscribe from feeds"
+get_feeds = "Get Feeds"
+help = "Help"
+add_source = "Add feed source"
+
+# ERRORS
+invalid = "Invalid command"
+invalid_data = "Invalid input"
+not_registered = "You are not registered send the\
+                 /start command to register"
+no_feed = "You havent subscribed to any feed"
+
+# HELP
+help_msg = """Contact @{admin} for any issues or visit"""
+
+# SUB UNSUB
+# sub = "Subscribe"
+# unsub = "Unsubscribe"
+
+# SUCCESS
+success = "{what} successful..."
+
+ask_fd_title = "Input the title of the feed: "
+ask_fd_url = "Input the RSS Feed URL: "
+ask_fd_public = "Public?"
