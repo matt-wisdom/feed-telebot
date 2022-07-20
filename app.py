d717dc294c1030c246bbc35ddce46dbbbbf13462
cRyp70s <matthewwisdom11@gmail.com>
2022-07-20 05:33:25 +0100

    Add main app

---
+++ app.py
@@ -0,0 +1,24 @@
+from asyncio.log import logger
+import json
+import asyncio
+from feedbot.database import create_all, FeedSource, session
+from feedbot.bot import create_bot
+import config
+
+
+async def main():
+    try:
+        create_all()
+        json_data = json.load(open(config.FEED_SRC_FILE))
+        for title, (url, desc) in json_data.items():
+            fd_src = FeedSource(public=True, url=url, title=title, description=desc)
+            session.add(fd_src)
+        session.commit()
+        bot = await create_bot()
+        await bot.run_until_disconnected()
+    except Exception as e:
+        logger.exception(e)
+        session.rollback()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
