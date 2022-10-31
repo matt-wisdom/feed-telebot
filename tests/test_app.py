6fdc944e9799ef04c7db091f5babaabad5ff82bd
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-10-31 01:08:01 +0100

    Add some tests

---
+++ tests/test_app.py
@@ -0,0 +1,5 @@
+from app import gather_feeds
+
+
+def test_app():
+    feeds = gather_feeds()
--- tests/test_feed_srcs.py
+++ tests/test_feed_srcs.py
@@ -4,10 +4,8 @@ from feedbot import feed_getters
 def test_get_feeds():
     # Test with src
     feed = feed_getters.get_feeds("https://stackabuse.com/rss/", "stackabuse")
-    # Feeds result must have two elements
     assert len(feed) == 2
 
-    # Title and image should be pressed
     assert len(feed[0]) == 2
     # Feed results
     if len(feed[1]) > 0:
@@ -17,10 +15,8 @@ def test_get_feeds():
     feed = feed_getters.get_feeds(
         "https://stackabuse.com/rss/",
     )
-    # Feeds result must have two elements
     assert len(feed) == 2
 
-    # Title and image should be pressed
     assert len(feed[0]) == 2
 
     # Feed results
